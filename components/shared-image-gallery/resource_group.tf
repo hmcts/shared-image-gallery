51874667a2bea7693216e179a1d841d47154a618
Zoe Cope <zoe.cope@hmcts.net>
2021-09-14 15:41:38 +0100

    add locals

--- components/shared-image-gallery/resource_group.tf
+++ components/shared-image-gallery/resource_group.tf
@@ -1,3 +1,7 @@
+locals {
+  common_tags = module.ctags.common_tags
+}
+
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment = var.env
