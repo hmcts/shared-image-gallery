6bd7a87480a377b0febb174b315ba4e3698db1dd
Zoe Cope <zoe.cope@hmcts.net>
2021-09-14 14:04:08 +0100

    remove types and update azurerm to 2.76.0

--- components/shared-image-gallery/variables.tf
+++ components/shared-image-gallery/variables.tf
@@ -1,22 +1,14 @@
 variable "env" {
   description = "Name of the environment to deploy the resource."
-  type        = string
-}
-variable "product" {
-  description = "Name of the product."
-  type        = string
-  default     = "shared-image-gallery"
 }
 
 variable "location" {
   description = "Azure location to deploy the resource"
-  type        = string
   default     = "UK South"
 }
 
 variable "builtFrom" {
   description = "Name of the GitHub repository this application is being built from."
-  type        = string
 }
 
 variable "resource_group_name" {
