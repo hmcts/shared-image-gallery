540f58182520a6190090963d342e0502d13006b1
Zoe Cope <zoe.cope@hmcts.net>
2021-09-17 15:13:37 +0100

    change image name

--- components/shared-image-gallery/image_gallery.tf
+++ components/shared-image-gallery/image_gallery.tf
@@ -6,8 +6,8 @@ resource "azurerm_shared_image_gallery" "image_gallery" {
   tags = module.ctags.common_tags
 }
 
-resource "azurerm_shared_image" "bastion-packer" {
-  name                = "bastion-packer"
+resource "azurerm_shared_image" "bastion-ubuntu" {
+  name                = "bastion-ubuntu"
   gallery_name        = azurerm_shared_image_gallery.image_gallery.name
   resource_group_name = azurerm_resource_group.image_gallery_rg.name
   location            = azurerm_resource_group.image_gallery_rg.location
