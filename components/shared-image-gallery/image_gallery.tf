92a98608d6138b5e0c89fdcc08ebd83c79358f96
Zoe Cope <zoe.cope@hmcts.net>
2021-09-17 15:01:36 +0100

    move to other file

--- components/shared-image-gallery/image_gallery.tf
+++ components/shared-image-gallery/image_gallery.tf
@@ -5,3 +5,17 @@ resource "azurerm_shared_image_gallery" "image_gallery" {
 
   tags = module.ctags.common_tags
 }
+
+resource "azurerm_shared_image" "bastion-packer" {
+  name                = "bastion-packer"
+  gallery_name        = azurerm_shared_image_gallery.image_gallery.name
+  resource_group_name = azurerm_resource_group.image_gallery_rg.name
+  location            = azurerm_resource_group.image_gallery_rg.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+  }
+}
