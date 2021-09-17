cfae559d5eeda598deb941c673c2ab79824c8fe5
Zoe Cope <zoe.cope@hmcts.net>
2021-09-17 14:55:43 +0100

    DTSPO-4723 Packer Image

---
+++ images/packer_image.tf
@@ -0,0 +1,14 @@
+resource "azurerm_shared_image" "bastion-packer" {
+  name                = "bastion-packer"
+  gallery_name        = azurerm_shared_image_gallery.image_gallery.name
+  resource_group_name = azurerm_resource_group.image_gallery_rg.name
+  location            = azurerm_resource_group.image_gallery_rg.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+  }
+}
+
