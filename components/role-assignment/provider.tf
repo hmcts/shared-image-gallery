767d006555412416a6baea4b02e2ba158ff03251
Enda Kelly <enda.kelly@hmcts.net>
2021-10-12 14:46:28 +0100

    terraform fmt

--- components/role-assignment/provider.tf
+++ components/role-assignment/provider.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias = "dts-management-prod-intsvc"
+  alias           = "dts-management-prod-intsvc"
   subscription_id = "2b1afc19-5ca9-4796-a56f-574a58670244"
   features {}
   skip_provider_registration = "true"
