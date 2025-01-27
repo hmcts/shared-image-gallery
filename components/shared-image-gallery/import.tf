d7c1998c8e94fb9af4054c7d131efeb95eba1adf
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-01-27 11:13:07 +0000

    Remove import

--- components/shared-image-gallery/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  id = "/subscriptions/2b1afc19-5ca9-4796-a56f-574a58670244/resourceGroups/hmcts-image-gallery-rg/providers/Microsoft.Compute/galleries/hmcts/images/jenkins-ubuntu-arm"
-  to = azurerm_shared_image.shared_image["jenkins-ubuntu-arm"]
-}
