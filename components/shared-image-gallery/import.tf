3123726f50d62e375712bc4fd8e591ef5e9e1869
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-01-27 10:25:22 +0000

    Fix import

--- components/shared-image-gallery/import.tf
+++ components/shared-image-gallery/import.tf
@@ -1,4 +1,4 @@
 import {
   id = "/subscriptions/2b1afc19-5ca9-4796-a56f-574a58670244/resourceGroups/hmcts-image-gallery-rg/providers/Microsoft.Compute/galleries/hmcts/images/jenkins-ubuntu-v2"
-  to = azurerm_shared_image.shared_image["jenkins-ubuntu-v2"].id
+  to = azurerm_shared_image.shared_image["jenkins-ubuntu-v2"]
 }
